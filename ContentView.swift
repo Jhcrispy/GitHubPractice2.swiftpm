3e1fa96d6a6d54b9c6b6533aeb7ede57b55ca0ee
Christopher Sebastian <csebastian8396@stu.d214.org>
2025-04-11 11:30:50 -0700

    Added a button that adds number1 and number2.

--- ContentView.swift
+++ ContentView.swift
@@ -3,11 +3,14 @@ import SwiftUI
 struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
-    @State var answer = ""
+    @State var answer: Int = 0
     
     var body: some View {
         TextField("Enter Number 1", value: $number1, format: .number)
         TextField("Enter Number 2", value: $number2, format: .number)
-        Text(answer)
+        Button("Add") {
+            answer = number1 + number2
+        }
+        Text(String(answer))
     }
 }
